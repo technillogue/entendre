9f1322babfe82619cf7bf0af5980dd283c59e69c
technillogue <wisepoison@gmail.com>
2020-01-25 17:33:23 -0500

    version

--- flask_app.py
+++ flask_app.py
@@ -5,14 +5,16 @@ import datetime
 app = Flask(__name__)
 
 
-template = '<html><head><title>entendre?</title></head><body>{}</body></html>'
+template = "<html><head><title>entendre?</title></head><body>{}</body></html>"
 input_field = '<form action="." method="POST"><textarea name="text"></textarea><input type="submit" value="add"></form>'
 
 metadata = sa.MetaData()
-msgs = sa.Table("msgs", metadata,
+msgs = sa.Table(
+    "msgs",
+    metadata,
     sa.Column("id", sa.Integer, primary_key=True),
     sa.Column("text", sa.Text),
-    sa.Column("created_at", sa.Date, default=datetime.datetime.now)
+    sa.Column("created_at", sa.Date, default=datetime.datetime.now),
 )
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="entendre",
@@ -23,14 +25,22 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 engine = sa.create_engine(SQLALCHEMY_DATABASE_URI)
 metadata.create_all(engine)
 
-@app.route('/', methods=["GET", "POST"])
-def main():
+
+@app.route("/", methods=["GET", "POST"])
+def main() -> str:
     try:
         conn = engine.connect()
         if request.method == "POST":
-            ins = sa.sql.text('''INSERT INTO msgs (text) VALUES (":text");''')
-            conn.execute(ins, text = request.form["text"])
-        msgs = "<br/>".join(str(row[0]) for row in conn.execute("SELECT text FROM msgs"))
+            ins = sa.sql.text("""INSERT INTO msgs (text) VALUES (":text");""")
+            conn.execute(ins, text=request.form["text"])
+        msgs = "<br/>".join(
+            str(row[0]) for row in conn.execute("SELECT text FROM msgs")
+        )
         return template.format(msgs + input_field)
     finally:
         conn.close()
+
+@app.route("/version")
+def version() -> str:
+    import sys
+    return sys.version
